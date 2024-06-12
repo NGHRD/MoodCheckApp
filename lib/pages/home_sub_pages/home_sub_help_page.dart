00392a215735e075fa985b7b756a6044f736553b
Niozk <99679240+Niozk@users.noreply.github.com>
2024-06-12 23:36:22 +0200

    Boven sectie compleet
    Alleen onder nog

--- lib/pages/home_sub_pages/home_sub_help_page.dart
+++ lib/pages/home_sub_pages/home_sub_help_page.dart
@@ -29,16 +29,16 @@ class HomeSubHelpPage extends StatelessWidget {
         title: const Text('Help & Informatie'),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16),
         child: ListView(
           children: [
             Container(
-              margin: const EdgeInsets.all(10.0), // You can customize the margin values
+              margin: const EdgeInsets.all(10),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    margin: const EdgeInsets.only(bottom: 4.0), // Adjust margin values as needed
+                    margin: const EdgeInsets.only(bottom: 4),
                     child: const Text(
                       'Voor levensbedreigende situaties',
                       style: TextStyle(fontSize: 18),
@@ -50,28 +50,136 @@ class HomeSubHelpPage extends StatelessWidget {
                       color: Color(0xFFEE7179),
                     ),
                   ),
-                  const SizedBox(height: 20),
+                  const SizedBox(height: 15),
+
+                  // const SizedBox(height: 20),
+                  // Container(
+                  //   margin: const EdgeInsets.only(bottom: 4),
+                  //   child: const Text(
+                  //     'Denk je aan zelfmoord?',
+                  //     style: TextStyle(fontSize: 18),
+                  //   ),
+                  // ),
+                  // Row(
+                  //   crossAxisAlignment: CrossAxisAlignment.start,
+                  //   children: [
+                  //     const Text('Bel 113'),
+                  //     SizedBox(width: 10),
+                  //     GestureDetector(
+                  //       onTap: () {
+                  //         _launchInBrowser(Uri.parse('https://www.113.nl/chatten'));
+                  //       },
+                  //       child: const Text('Chat via: https://www.113.nl/chatten'),
+                  //     ),
+                  //   ],
+                  // ),
+
                   Container(
-                    margin: const EdgeInsets.only(bottom: 4.0), // Adjust margin values as needed
+                    margin: const EdgeInsets.only(bottom: 4),
                     child: const Text(
                       'Denk je aan zelfmoord?',
                       style: TextStyle(fontSize: 18),
                     ),
                   ),
-                  const Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('Bel 113'),
-                    ],
-                  )
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    child: const Text(
+                      'Bel 113',
+                      style: TextStyle(
+                        color: Color(0xFF7D7B79),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 50),
+                    child: const Text(
+                      'Chat via:',
+                      style: TextStyle(
+                        color: Color(0xFF7D7B79),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 75),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 4),
+                          child: Icon(Icons.link_outlined),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _launchInBrowser(Uri.parse('https://www.113.nl/chatten'));
+                          },
+                          child: const Text(
+                            'https://www.113.nl/chatten',
+                            style: TextStyle(
+                              color: Color(0xFF7D7B79),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  const SizedBox(height: 15),
 
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    child: const Text(
+                      'Een luisterend oor?',
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(bottom: 4),
+                    child: GestureDetector(
+                      onTap: () {
+                        _makePhoneCall('31880767000');
+                      },
+                      child: const Text(
+                        'Bel 088 0767 000',
+                        style: TextStyle(
+                          color: Color(0xFF7D7B79),
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 50),
+                    child: const Text(
+                      'Chat via:',
+                      style: TextStyle(
+                        color: Color(0xFF7D7B79),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.only(left: 75),
+                    child: Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.only(right: 4.0),
+                          child: Icon(Icons.link_outlined),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            _launchInBrowser(Uri.parse('https://deluisterlijn.nl/'));
+                          },
+                          child: const Text(
+                            'https://deluisterlijn.nl/',
+                            style: TextStyle(
+                              color: Color(0xFF7D7B79),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ]
               ),
             ),
 
-            const SizedBox(height: 5),
-            const Text('Een luisterend oor?'),
-            const SizedBox(height: 5),
             const Text('Meer over mentale zorg'),
             const SizedBox(height: 20),
             ListTile(
@@ -82,10 +190,10 @@ class HomeSubHelpPage extends StatelessWidget {
               title: const Text('Chat via: https://deluisterlijn.nl/'),
               onTap: () => _launchInBrowser(Uri.parse('https://deluisterlijn.nl/')),
             ),
-            ListTile(
-              title: const Text('Chat via: https://www.113.nl/chatten'),
-              onTap: () => _launchInBrowser(Uri.parse('https://www.113.nl/chatten')),
-            ),
+            // ListTile(
+            //   title: const Text('Chat via: https://www.113.nl/chatten'),
+            //   onTap: () => _launchInBrowser(Uri.parse('https://www.113.nl/chatten')),
+            // ),
             ListTile(
               title: const Text('https://moetiknaardedokter.nl/klacht/psychische-klachten'),
               onTap: () => _launchInBrowser(Uri.parse('https://moetiknaardedokter.nl/klacht/psychische-klachten')),
